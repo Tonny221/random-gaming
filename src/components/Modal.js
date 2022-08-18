34f2bdcadd1caa3d5b42f43d929173b5322f8433
Tonny221 <anthonny14gabriell@gmail.com>
2022-08-17 21:28:31 -0400

    modal pc finished

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,33 +2,37 @@ import styles from './Modal.module.css';
 
 function Modal() {
     return (
-        <div className={styles.modal}>
-            <div className={styles.overlay}>
-            </div>
-            <div className={styles.content}>
-                <h2>Nome</h2>
-                <div>
-                    <div>
-                        <img src="#" alt='profile-pic' />
+        <div className={styles.container}>
+            <div className={styles.modal}>
+                <div className={styles.overlay}>
+                </div>
+                <div className={styles.content}>
+                    <h2>Nome</h2>
+                    <div className={styles.personalData}>
+                        <div className={styles.profilePic}>
+                            <img src="#" alt='profile-pic' />
+                        </div>
+                        <div>
+                            <h4>Cargo:</h4>
+                            <h4>Email:</h4>
+                            <h4>CPF:</h4>
+                            <h4>Data de nascimento:</h4>
+                            <h4>Celular:</h4>
+                        </div>
                     </div>
-                    <h4>Cargo:</h4>
-                    <h4>Email:</h4>
-                    <h4>CPF:</h4>
-                    <h4>Data de nascimento:</h4>
-                    <h4>Celular:</h4>
+
+                    <h2>Endereço</h2>
+
+                    <div className={styles.adress}>
+                        <h4>CEP:</h4>
+                        <h4>Estado:</h4>
+                        <h4>Cidade:</h4>
+                        <h4>Bairro:</h4>
+                        <h4>Rua:</h4>
+                        <h4>Número:</h4>
+                    </div>
+
                 </div>
-
-                <h2>Endereço</h2>
-
-                <div>
-                    <h4>CEP:</h4>
-                    <h4>Estado:</h4>
-                    <h4>Cidade:</h4>
-                    <h4>Bairro:</h4>
-                    <h4>Rua:</h4>
-                    <h4>Número:</h4>
-                </div>
-
             </div>
         </div>
     );
